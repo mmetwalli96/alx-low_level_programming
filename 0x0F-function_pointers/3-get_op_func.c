10f01207f69ae4d9964d8926e9272e227ea6535a
Mohamed Metwalli <m.metwalli@outlook.com>
2023-07-19 20:55:32 -0400

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,18 +1,12 @@
 #include "3-calc.h"
+
 /**
- * get_op_func - function that selects the correct function to perform the
- *               operation asked by the user.
- *
- * @s: This is the input operator
- *
- * Return: This function returns a pointer to the function that corresponds to
- *         the operator given as a parameter. Example: get_op_func("+")
- *         should return a pointer to the function op_add"")
- *         If s does not match any of the 5 expected operators
- *         (+, -, *, /, %),return NULL)
- *
+ * get_op_func - compares the struct and proceed to operates.
+ * @s: operator string.
+ * Return: result.
  */
-int (*get_op_func(char *s))(int a, int b)
+
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -22,14 +16,14 @@ int (*get_op_func(char *s))(int a, int b)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int i;
 
-	while (ops[i].op != NULL)
+	i = 0;
+
+	while (i < 5)
 	{
-		if (*ops[i].op == *s)
-		{
+		if (*s == *ops[i].op)
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (NULL);
