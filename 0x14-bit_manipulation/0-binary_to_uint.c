ec984a1be659df587fc998d5f17d91f8ab171640
Mohamed Metwalli <m.metwalli@outlook.com>
2023-08-03 20:38:34 -0400

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,31 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b) {
-    unsigned int result = 0;
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int len, base_two;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (int i = 0; b[i] != '\0'; i++) {
-        if (b[i] == '0') {
-            result = result * 2;
-        } else if (b[i] == '1') {
-            result = result * 2 + 1;
-        } else {
-            return 0; 
-        }
-    }
+	ui = 0;
 
-    return result;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
